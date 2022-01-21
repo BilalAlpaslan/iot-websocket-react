8bcf921961128c8d32be1e21def00ea43cd626a8
bilalAlpaslan <m.bilal.alpaslan@gmail.com>
2022-01-21 20:35:27 +0300

    cleaned

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,39 +1,48 @@
 import React, { Component } from "react";
 import { w3cwebsocket as W3CWebSocket } from "websocket";
-import Page from "./page";
+import Model from "./model";
 
-var client = new W3CWebSocket("ws://0.0.0.0:5678");
+var client = new W3CWebSocket("ws://192.168.1.104:8001");
 
 class App extends Component {
   state = {
     data: [],
   };
 
-  buttonClick=(a)=> client.send(a)
+  buttonClick = (data) => client.send(data)
 
   componentDidMount() {
-    client.onopen = () => {
-      console.log("WebSocket Connected");
-    };
+    client.onopen = () => console.log("WebSocket Connected");
+
     client.onmessage = (message) => {
-      if(Number.isInteger(parseInt(message.data))){
+      if (Number.isInteger(parseInt(message.data))) {
         this.setState({ data: this.state.data.concat([message.data]) });
-        if (this.state.data.length > 9){
+
+        if (this.state.data.length > 9)
           this.setState({ data: this.state.data.slice(1, this.state.data.length) });
+
       }
-      }
-      
     };
   }
 
   render() {
     return (
       <div>
-        <button onClick={()=>this.buttonClick(97)}>tıkla</button>
-        <button onClick={()=>this.buttonClick(98)}>tıkla</button>
-        <Page data={this.state.data} />
+        <button onClick={() => this.buttonClick(97)}>tıkla</button>
+        <button onClick={() => this.buttonClick(98)}>tıkla</button>
+        <div className={"row"} style={{ backgroundColor: "#404040", height: "95vh", width: "200vh" }}>
+          <div className={"col-md-6"} style={{ padding: 40 }}>
+            {this.state.data.map(data =>
+              <div key={data} style={{ color: "white", fontSize: 40 }}>  {data}  </div>
+            )}
+          </div>
+          <div className={"col-md-6"} style={{ backgroundColor: "#808080", padding: 40 }}>
+            <div style={{ height: "200", width: "200" }}>
+              {/* <Model angle={data%180}/> */}
+            </div>
+          </div>
+        </div>
       </div>
-      
     );
   }
 }
